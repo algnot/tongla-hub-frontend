76a4ab352daff186965c96e6fd697d27c3f25439
algnot <thanawat.tongla@gmail.com>
2025-05-18 19:20:05 +0700

    fix: fix up

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,21 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
-import React, { useEffect } from 'react'
-import { useSearchParams } from 'next/navigation'
-import { OpenIdClient } from "@/lib/request";
-export default function page() {
+import React, { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { OpenIdClient } from "@/lib/open_id";
+export default function Page() {
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+  const openIdClient = new OpenIdClient();
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
-    const searchParams = useSearchParams();
-    const code = searchParams.get("code");
-    const openIdClient = new OpenIdClient();
-    useEffect(() => {
-        getUserInfo()
-    }, [])
-    
-    const getUserInfo = async() => {
-        const token = await openIdClient.getToken(code?? "");
-        const userData = await openIdClient.getUserInfo(token.id_token);
-    }
-  return (
-    <div>{code}</div>
-  )
+  const getUserInfo = async () => {
+    const token = await openIdClient.getToken(code ?? "");
+    const userData = await openIdClient.getUserInfo(token.id_token);
+    console.log(userData);
+  };
+  
+  return <div>This feature is implementing..</div>;
 }
